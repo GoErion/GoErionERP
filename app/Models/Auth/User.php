b259be64eb97611af06a6c781624b7c7b39ba3e4
GoErion <goerioninc@gmail.com>
2026-09-10 19:59:57 +0300

    fix dev setup

--- app/Models/Auth/User.php
+++ app/Models/Auth/User.php
@@ -1,27 +1,29 @@
 <?php
+declare(strict_types=1);
 
-namespace App\Models;
+namespace App\Models\Auth;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Traits\HasUuid;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Attributes\RouteKey;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
+#[RouteKey('uuid')]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use Notifiable;
+    use HasUuid;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    protected $table = 'users';
+
     protected function casts(): array
     {
         return [
---
+++ app/Traits/HasUuid.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+trait HasUuid
+{
+    use HasUuids;
+    public function uniqueIds(): array
+    {
+        return ['uuid'];
+    }
+}
